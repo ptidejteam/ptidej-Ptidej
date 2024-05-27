5c5fd2745df254915b577c909ebc73b952343c7b
baptiste2k8 <baptiste2k8@gmail.com>
2024-05-27 16:40:54 -0400

    Modified Test1.java (in padl.creator.javafile.javac.test.simple) in PADL Creator JavaFile (JavaC)

--- PADL Creator JavaFile (JavaC)/src/test/java/padl/creator/javafile/javac/test/simple/Test1.java
+++ PADL Creator JavaFile (JavaC)/src/test/java/padl/creator/javafile/javac/test/simple/Test1.java
@@ -33,6 +33,8 @@ import padl.kernel.impl.Factory;
 public class Test1 extends TestCase {
 	private static final String PATH = "../PADL Creator JavaFile (JavaC)/target/test-classes/";
 	private static final String FILE = Test1.PATH + "CreatorJava.java";
+	private static final String TEST_B = "*\n * This class is used to create a PADL Model, from a set of Java Source Files, using JCT.\n *\n * @author Mathieu Lemoine\n ";
+	private static final String TEST_C = "*\n	 * @param files List of Path to each of the java source file want to put in the PADL Model.\n	 * @param diag DiagnosticListener used to report error during compilation pass.\n	 * @param options Options to pass to JavaC, splited as in a command line.\n	 ";
 
 	private static ICodeLevelModel CodeLevelModel;
 
@@ -43,45 +45,33 @@ public class Test1 extends TestCase {
 	protected void setUp() {
 		if (Test1.CodeLevelModel == null) {
 			try {
-				Test1.CodeLevelModel = Factory.getInstance()
-						.createCodeLevelModel("");
-				Test1.CodeLevelModel.create(new JavaFileCreator(Test1.PATH,
-						new String[] { Test1.FILE }));
-			}
-			catch (final CreationException e) {
+				Test1.CodeLevelModel = Factory.getInstance().createCodeLevelModel("");
+				Test1.CodeLevelModel.create(new JavaFileCreator(Test1.PATH, new String[] { Test1.FILE }));
+			} catch (final CreationException e) {
 				e.printStackTrace();
 			}
 		}
 	}
 
 	public void testA() {
-		Assert.assertEquals("Number of classes", 1,
-				Test1.CodeLevelModel.getNumberOfTopLevelEntities(IClass.class));
+		Assert.assertEquals("Number of classes", 1, Test1.CodeLevelModel.getNumberOfTopLevelEntities(IClass.class));
 	}
 
 	public void testB() {
-		final IFirstClassEntity entity = Test1.CodeLevelModel
-				.getTopLevelEntityFromID("padl.creator.CreatorJava");
+		final IFirstClassEntity entity = Test1.CodeLevelModel.getTopLevelEntityFromID("padl.creator.CreatorJava");
 		Assert.assertNotNull("Class CreatorJava does not exist", entity);
-		Assert.assertEquals("Class CreatorJava name", "CreatorJava",
-				entity.getDisplayName());
-		final String comment = entity.getComment().replaceAll("\\r\\n?", "\\n");
-		Assert.assertEquals("Class CreatorJava comment",
-				"*\n * This class is used to create a PADL Model, from a set of Java Source Files, using JCT.\n *\n * @author Mathieu Lemoine\n ",
+		Assert.assertEquals("Class CreatorJava name", "CreatorJava", entity.getDisplayName());
+		final String comment = entity.getComment().replaceAll("\\r", "").replaceAll("\\n", "");
+		Assert.assertEquals("Class CreatorJava comment", Test1.TEST_B.replaceAll("\\r", "").replaceAll("\\n", ""),
 				comment);
 	}
 
 	public void testC() {
-		final IFirstClassEntity entity = Test1.CodeLevelModel
-				.getTopLevelEntityFromID("padl.creator.CreatorJava");
+		final IFirstClassEntity entity = Test1.CodeLevelModel.getTopLevelEntityFromID("padl.creator.CreatorJava");
 		final IConstructor constructor = (IConstructor) entity
-				.getConstituentFromID(
-						"<init>(javax.tools.DiagnosticListener, java.lang.Iterable, Ljava.io.File)");
+				.getConstituentFromID("<init>(javax.tools.DiagnosticListener, java.lang.Iterable, Ljava.io.File)");
 		Assert.assertNotNull("Constructor does not exist", constructor);
-		final String comment = constructor.getComment().replaceAll("\\r\\n?",
-				"\\n");
-		Assert.assertEquals("Constructor comment",
-				"*\n	 * @param files List of Path to each of the java source file want to put in the PADL Model.\n	 * @param diag DiagnosticListener used to report error during compilation pass.\n	 * @param options Options to pass to JavaC, splited as in a command line.\n	 ",
-				comment);
+		final String comment = constructor.getComment().replaceAll("\\r", "").replaceAll("\\n", "");
+		Assert.assertEquals("Constructor comment", Test1.TEST_C.replaceAll("\\r", "").replaceAll("\\n", ""), comment);
 	}
 }
