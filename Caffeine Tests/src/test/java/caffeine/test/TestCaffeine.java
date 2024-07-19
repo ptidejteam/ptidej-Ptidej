ed710b8db4452970f877b4e81063ba8f00d74976
VishnuRameshbabu <vishnuramesh200@gmail.com>
2024-07-19 15:18:07 -0400

    Updated pom.xml to skip tests

--- Caffeine Tests/src/test/java/caffeine/test/TestCaffeine.java
+++ Caffeine Tests/src/test/java/caffeine/test/TestCaffeine.java
@@ -27,55 +27,38 @@ public final class TestCaffeine extends junit.framework.TestSuite {
 	public TestCaffeine() {
 		super();
 	}
+
 	public TestCaffeine(final Class theClass) {
 		super(theClass);
 	}
+
 	public TestCaffeine(final String name) {
 		super(name);
 	}
+
 	public static Test suite() {
 		final TestCaffeine suite = new TestCaffeine();
 
-			suite.addTest(new TestSuite(
-				caffeine.test.aggregation3.CaffeineLauncher.class));
-			suite
-				.addTest(new TestSuite(caffeine.test.array.CaffeineLauncher.class));
-			suite.addTest(new TestSuite(
-				caffeine.test.association1.CaffeineLauncher.class));
-			suite.addTest(new TestSuite(
-				caffeine.test.association2.CaffeineLauncher.class));
-			suite.addTest(new TestSuite(
-				caffeine.test.collection.CaffeineLauncher.class));
-			suite.addTest(new TestSuite(
-				caffeine.test.composition1.CaffeineLauncher.class));
-			suite.addTest(new TestSuite(
-				caffeine.test.composition2.CaffeineLauncher.class));
-		suite.addTest(new TestSuite(
-			caffeine.test.composition3.CaffeineLauncher.class));
-		suite.addTest(new TestSuite(
-			caffeine.test.composition4.CaffeineLauncher.class));
-			suite.addTest(new TestSuite(
-				caffeine.test.composition5.CaffeineLauncher.class));
-			suite.addTest(new TestSuite(
-				caffeine.test.composition6.counterexample.CaffeineLauncher.class));
-			suite.addTest(new TestSuite(
-				caffeine.test.composition7.counterexample.CaffeineLauncher.class));
-			suite.addTest(new TestSuite(
-				caffeine.test.composition8.counterexample.CaffeineLauncher.class));
-			suite.addTest(new TestSuite(
-				caffeine.test.composition9.CaffeineLauncher.class));
-			suite.addTest(new TestSuite(
-				caffeine.test.interactiondiagram1.CaffeineLauncher.class));
-			suite.addTest(new TestSuite(
-				caffeine.test.interactiondiagram2.CaffeineLauncher.class));
-			suite.addTest(new TestSuite(
-				caffeine.test.multithreading1.CaffeineLauncher.class));
-			suite.addTest(new TestSuite(
-				caffeine.test.multithreading2.CaffeineLauncher.class));
-			suite.addTest(new TestSuite(
-				caffeine.test.nextEvent2.CaffeineLauncher.class));
-			suite.addTest(new TestSuite(
-				caffeine.test.nextEvent3.CaffeineLauncher.class));
+		suite.addTest(new TestSuite(caffeine.test.aggregation3.CaffeineLauncher.class));
+		suite.addTest(new TestSuite(caffeine.test.array.CaffeineLauncher.class));
+		suite.addTest(new TestSuite(caffeine.test.association1.CaffeineLauncher.class));
+		suite.addTest(new TestSuite(caffeine.test.association2.CaffeineLauncher.class));
+		suite.addTest(new TestSuite(caffeine.test.collection.CaffeineLauncher.class));
+		suite.addTest(new TestSuite(caffeine.test.composition1.CaffeineLauncher.class));
+		suite.addTest(new TestSuite(caffeine.test.composition2.CaffeineLauncher.class));
+		suite.addTest(new TestSuite(caffeine.test.composition3.CaffeineLauncher.class));
+		suite.addTest(new TestSuite(caffeine.test.composition4.CaffeineLauncher.class));
+		suite.addTest(new TestSuite(caffeine.test.composition5.CaffeineLauncher.class));
+		suite.addTest(new TestSuite(caffeine.test.composition6.counterexample.CaffeineLauncher.class));
+		suite.addTest(new TestSuite(caffeine.test.composition7.counterexample.CaffeineLauncher.class));
+		suite.addTest(new TestSuite(caffeine.test.composition8.counterexample.CaffeineLauncher.class));
+		suite.addTest(new TestSuite(caffeine.test.composition9.CaffeineLauncher.class));
+		suite.addTest(new TestSuite(caffeine.test.interactiondiagram1.CaffeineLauncher.class));
+		suite.addTest(new TestSuite(caffeine.test.interactiondiagram2.CaffeineLauncher.class));
+		suite.addTest(new TestSuite(caffeine.test.multithreading1.CaffeineLauncher.class));
+		suite.addTest(new TestSuite(caffeine.test.multithreading2.CaffeineLauncher.class));
+		suite.addTest(new TestSuite(caffeine.test.nextEvent2.CaffeineLauncher.class));
+		suite.addTest(new TestSuite(caffeine.test.nextEvent3.CaffeineLauncher.class));
 
 		return suite;
 	}
