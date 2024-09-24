ec447bea412d97c73eb05bbd1e2197b3e336603a
Belal Ahmad Nwiran <bnwiran@gmail.com>
2024-09-24 15:51:03 -0400

    Fix Caffeine tests
    - Copy Query.pl and Rules.pl from Caffeine to resources.
    - Fix their paths in initialize and start methods in
      caffeine.simulator.Caffeine

--- Caffeine/src/main/java/caffeine/Caffeine.java
+++ Caffeine/src/main/java/caffeine/Caffeine.java
@@ -16,7 +16,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringReader;
 import java.io.Writer;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
