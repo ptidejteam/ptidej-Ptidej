bd9ab1771c49a4e2ffdb8f0a05d3887ad960ae77
iman.tra96 <iman.tra96@gmail.com>
2024-11-04 09:15:01 -0500

    Testing

---
+++ CPL/src/test/java/util/lang/test/CFParseBCELConvertorTestSuperName.java
@@ -0,0 +1,66 @@
+package util.lang.test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.bcel.classfile.ClassParser;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.ibm.toad.cfparse.ClassFile;
+
+import util.lang.CFParseBCELConvertor;
+import util.lang.CFParseBCELConvertorSuperName;
+
+class CFParseBCELConvertorTestSuperName {
+	private ClassFile classFile_CFParse_Original;
+	private ClassFile classFile_CFParse_Converted;
+
+	@BeforeEach
+	void setUp() throws IOException {
+		final String classFile_Path = "../CPL/target/test-classes/Random ClassFiles/NameDialog.class";
+
+		this.classFile_CFParse_Original = new ClassFile(
+				new FileInputStream(classFile_Path));
+
+		this.classFile_CFParse_Converted = CFParseBCELConvertorSuperName
+				.convertClassFile(
+						new ClassParser(new FileInputStream(classFile_Path), "")
+								.parse());
+	}
+
+	@Test
+	void testGetAccess() {
+		assertEquals(this.classFile_CFParse_Original.getAccess(),
+				this.classFile_CFParse_Converted.getAccess());
+	}
+
+	@Test
+	void testGetAttrs() {
+		// assertEquals(this.classFile_CFParse_Original.getAttrs(), this.classFile_CFParse_Converted.getAttrs());
+	}
+	@Test
+	void testGetSuperName() {
+		assertEquals(this.classFile_CFParse_Original.getSuperName(), this.classFile_CFParse_Converted.getSuperName());
+	}
+	@Test
+	void testGetSourceFileName() {
+		assertEquals(this.classFile_CFParse_Original.getSourceFilename(), this.classFile_CFParse_Converted.getSourceFilename());
+	}
+
+	@Test
+	void testGetName() {
+		assertEquals(this.classFile_CFParse_Original.getName(),
+				this.classFile_CFParse_Converted.getName());
+	}
+
+	@Test
+	void testGetFields() {
+		assertEquals(
+				this.classFile_CFParse_Original.getFields().get(0).toString(),
+				this.classFile_CFParse_Converted.getFields().get(0).toString());
+	}
+
+}
