814b3589a3d5efe1b6ae72bb4a33c996c47be38c
amr-abdalla <amr_alaa2@aucegypt.edu>
2025-12-04 11:13:50 -0500

    Added BFSTest and Fixed UML Generator Test1

--- PADL Analyses/src/test/java/padl/analysis/plantUMLGenerator/test/BFSTest.java
+++ PADL Analyses/src/test/java/padl/analysis/plantUMLGenerator/test/BFSTest.java
@@ -7,7 +7,8 @@ import java.io.IOException;
 import java.sql.Timestamp;
 
 import org.junit.Assert;
-
+import padl.analysis.plantUMLGenerator.BFSGenerator;
+import padl.analysis.plantUMLGenerator.BFSPlantUMLGenerator;
 import junit.framework.TestCase;
 import padl.analysis.UnsupportedSourceModelException;
 import padl.analysis.plantUMLGenerator.PlantUMLGenerator;
@@ -20,21 +21,22 @@ import padl.kernel.impl.Factory;
 import util.io.ProxyConsole;
 
 /**
- * @author Vishnu Rameshbabu
- * @since 2024/05/10
+ * @author Amr Abdalla
+ * @since 2025/12/04
  */
-public class Test1 extends TestCase {
+public class BFSTest extends TestCase {
 
 	private static ICodeLevelModel intendedCodeLevelModel;
 	private static IIdiomLevelModel intendedIdiomLevelModel;
 	private static String generatedUMLContent;
 	private static String testAgainstUMLContent;
-	private static String filePath = "../DeMIMA/target/test-classes/ptidej/example/composite2/";
-	private static String plantUMLGeneratedFiles = "../PADL Analyses/src/test/java/padl/analysis/plantUMLGenerator/test/exampleFile/composite2_example_2024-05-07.txt";
-	private static String plantUMLGeneratedFileIncorrect1 = "../PADL Analyses/src/test/java/padl/analysis/plantUMLGenerator/test/exampleFile/composite2_example_2024-05-07.txt";
+	private static String filePath = "../PADL Analyses/target/test-classes/padl/example/composite2/";
+	// TODO Use regular temporary file from ProxyDisk
+	private static String plantUMLGeneratedFiles = "../PADL Analyses/src/test/java/padl/analysis/plantUMLGenerator/test/exampleFile/composite2_BFS_example_2025-12-4.txt";
+	private static String plantUMLGeneratedFileIncorrect1 = "../PADL Analyses/src/test/java/padl/analysis/plantUMLGenerator/test/exampleFile/composite2_BFS_example_2025-12-4.txt";
 	private static String testAgainstPlantUMLGeneratedFileIncorrect1;
 
-	public Test1(final String testName) {
+	public BFSTest(final String testName) {
 		super(testName);
 	}
 
@@ -50,8 +52,8 @@ public class Test1 extends TestCase {
 			}
 			if (readText != null)
 				readText.close();
-			Test1.testAgainstUMLContent = textBuilder.toString();
-			Test1.testAgainstUMLContent = Test1.testAgainstUMLContent
+			BFSTest.testAgainstUMLContent = textBuilder.toString();
+			BFSTest.testAgainstUMLContent = BFSTest.testAgainstUMLContent
 					.replace("\r", "").replaceFirst("[\n]+$", "");
 			readText = new BufferedReader(
 					new FileReader(new File(plantUMLGeneratedFileIncorrect1)));
@@ -63,31 +65,31 @@ public class Test1 extends TestCase {
 			}
 			if (readText != null)
 				readText.close();
-			Test1.testAgainstPlantUMLGeneratedFileIncorrect1 = textBuilder
+			BFSTest.testAgainstPlantUMLGeneratedFileIncorrect1 = textBuilder
 					.toString();
-			Test1.testAgainstPlantUMLGeneratedFileIncorrect1 = Test1.testAgainstPlantUMLGeneratedFileIncorrect1
+			BFSTest.testAgainstPlantUMLGeneratedFileIncorrect1 = BFSTest.testAgainstPlantUMLGeneratedFileIncorrect1
 					.replace("\r", "").replaceFirst("[\n]+$", "");
 			ProxyConsole.getInstance().normalOutput()
 					.println("Test against UML Content");
 			ProxyConsole.getInstance().normalOutput()
 					.println(testAgainstUMLContent);
-			Test1.intendedCodeLevelModel = Factory.getInstance()
+			BFSTest.intendedCodeLevelModel = Factory.getInstance()
 					.createCodeLevelModel("");
-			Test1.intendedCodeLevelModel.create(new CompleteClassFileCreator(
+			BFSTest.intendedCodeLevelModel.create(new CompleteClassFileCreator(
 					new String[] { filePath }, true));
 			intendedIdiomLevelModel = (IIdiomLevelModel) new AACRelationshipsAnalysis()
 					.invoke(intendedCodeLevelModel);
-			PlantUMLGenerator PlantUMLGeneratorNew = new PlantUMLGenerator();
+			BFSPlantUMLGenerator PlantUMLGeneratorNew = new BFSPlantUMLGenerator(new PlantUMLGenerator());
 			intendedIdiomLevelModel.generate(PlantUMLGeneratorNew);
 			String umlContent = (String) PlantUMLGeneratorNew.getCode();
 			String timeStampForUMLTextFile = new Timestamp(
 					System.currentTimeMillis()).toString().split(" ")[0];
 			System.out.println(timeStampForUMLTextFile);
-			Test1.generatedUMLContent = umlContent;
+			BFSTest.generatedUMLContent = umlContent;
 			ProxyConsole.getInstance().normalOutput()
 					.println("Generated UML Content");
 			ProxyConsole.getInstance().normalOutput()
-					.println(Test1.generatedUMLContent);
+					.println(BFSTest.generatedUMLContent);
 		}
 		catch (CreationException e) {
 			e.printStackTrace();
@@ -102,13 +104,13 @@ public class Test1 extends TestCase {
 	}
 
 	public void testGeneratedUMLContent() {
-		Assert.assertEquals(Test1.testAgainstUMLContent, generatedUMLContent);
-		Assert.assertNotEquals(Test1.testAgainstUMLContent,
+		Assert.assertEquals(BFSTest.testAgainstUMLContent, generatedUMLContent);
+		Assert.assertNotEquals(BFSTest.testAgainstUMLContent,
 				(generatedUMLContent + "test"));
 	}
 
 	public void testGeneratedUMLContentIncorrect() {
-		Assert.assertNotEquals(Test1.testAgainstPlantUMLGeneratedFileIncorrect1,
+		Assert.assertNotEquals(BFSTest.testAgainstPlantUMLGeneratedFileIncorrect1,
 				(generatedUMLContent + "test"));
 	}
 
