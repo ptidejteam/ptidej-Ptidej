1235124b09622e48a3138bc4e8a0816a67cd5f87
amr-abdalla <amr_alaa2@aucegypt.edu>
2026-05-07 18:56:25 -0400

    Changed Cardinality from enum to class

--- PADL/src/main/java/padl/kernel/Cardinality.java
+++ PADL/src/main/java/padl/kernel/Cardinality.java
@@ -1,17 +1,37 @@
 package padl.kernel;
 
-public enum Cardinality {
-	One, Many;
+public final class Cardinality {
+
+	public static final Cardinality One = new Cardinality(1);
+	public static final Cardinality Many = new Cardinality(2);
+
+	private final int value;
+
+	private Cardinality(final int value) {
+		this.value = value;
+	}
+
+	public int getValue() {
+		return this.value;
+	}
 
 	public String toString() {
-		// Amr 26/05/05: Ugly!
-		// But it makes the tests pass...
-		// TODO Remove and update the tests
-
-		if (this == One) {
-			return "1";
-		} else {
-			return "2";
+		return Integer.toString(this.value);
+	}
+	
+	public static Cardinality valueOf(final String name) {
+		if (name == null) {
+			throw new NullPointerException("Name cannot be null");
 		}
+
+		if (name.equals("One")) {
+			return One;
+		}
+		else if (name.equals("Many")) {
+			return Many;
+		}
+
+		throw new IllegalArgumentException(
+			"No enum constant padl.kernel.Cardinality." + name);
 	}
 }
