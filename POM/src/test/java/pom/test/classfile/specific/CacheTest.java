a0d315b3c0f8e7d1014a9f4c97df58ccb7249ebb
gndayisenga <gilbertndayisengaa@gmail.com>
2026-08-20 19:08:50 +0200

    Fix BOM encoding in CacheTest.java (use UTF-8 without BOM)

--- POM/src/test/java/pom/test/classfile/specific/CacheTest.java
+++ POM/src/test/java/pom/test/classfile/specific/CacheTest.java
@@ -1,4 +1,4 @@
-﻿/*******************************************************************************
+/*******************************************************************************
  * Copyright (c) 2001-2014 Yann-Ga├½l Gu├⌐h├⌐neuc and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the GNU Public License v2.0
