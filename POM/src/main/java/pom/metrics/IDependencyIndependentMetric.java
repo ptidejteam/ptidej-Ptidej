8c7ec6d57e4d9d65bdacd0e6f433b2c53e659834
gndayisenga <gilbertndayisengaa@gmail.com>
2026-08-24 13:29:21 +0200

    Fix Issue #412: decouple IDependencyIndependentMetric from IUnaryMetric

--- POM/src/main/java/pom/metrics/IDependencyIndependentMetric.java
+++ POM/src/main/java/pom/metrics/IDependencyIndependentMetric.java
@@ -12,6 +12,7 @@ package pom.metrics;
 
 // Tagging interface for model independent metrics (like LCOM...)
 // TODO It could be more interesting to have IUnary (truly), IBinary, and INary metrics.
-public interface IDependencyIndependentMetric extends IUnaryMetric {
+public interface IDependencyIndependentMetric extends IMetric {
+	
 
 }
